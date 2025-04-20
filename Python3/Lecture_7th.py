79cf53cc6785d2c35488d1f1fa8fc59192ed8d93
Rishikesh-001 <rishikeshraj4409@gmail.com>
2025-04-20 18:28:04 +0530

    Learned about file Input and Output

--- Python3/Lecture_7th.py
+++ Python3/Lecture_7th.py
@@ -89,13 +89,97 @@ with open("Python3\\sammple.txt","w") as f:
 '''
 
 # WAP to delete file sammple.txt file
-
+'''
 import os
-os.remove("Python3\\sammple.txt")
+os.remove("Python3\\sammple.txt") # The sample file have been deleted
+'''
+
+#<<<<-------------------------------------Lets Practice--------------------------------------->>>>
+
+# Ceate a new file "Practice.txt" using Python. Add the following data in it...
+'''
+f = open("Python3\\Practice.txt","w")
+f.write("Hi everyone\nWe are learning File I/O\nusing Java.\nI like programming in Java.")
+f.close()
+'''
+
+# WAF that replace all occurances of "Java" with "Python" in above file...
+'''
+with open("Python3\\Practice.txt","r") as f:
+    data = f.read()
+
+new_data = data.replace("Java","Python")
+print(new_data)
+
+with open("Python3\\Practice.txt","w") as f:
+    f.write(new_data)
+'''
+# Search if the word learning is present in the file or not...
+'''
+with open("Python3\\Practice.txt","r") as f:
+    data = f.read()
+    if(data.find("learning") != -1):
+        print("Found")
+    else:
+        print("Not Found")
+'''
+
+# Write the above program as a function
+
+'''
+def check_for_word():
+    word = "learning"
+    with open("Python3\\Practice.txt","r") as f:
+        data = f.read()
+        if(data.find(word) != -1):
+            print("Found")
+        else:
+            print("Not Found")
+    
+check_for_word()
+'''
+
+# WAF to find in which line of the file does the word learning occur first, Print -1 if not found
+'''
+def check_word_byline():
+    word = str(input("Enter word to find in file:"))
+    data = True
+    line_no = 1
+    with open("Python3\\Practice.txt","r") as f:
+        while data:
+            data = f.readline()
+            if(word in data):
+                print(line_no)
+                return
+            line_no += 1
+    return -1
+
+print(check_word_byline())
+'''
 
 
+# From a file containing numbers seperated by comma, print the count of even numbers...
+'''
+count = 0
+with open("Python3\\Numbers.txt","r") as f:
+    data = f.read()
 
+    num = data.split(",")
+    for val in num:
+        if(int(val) % 2 == 0):
+            count += 1
+print(count)
+'''
+# From a file containing numbers seperated by comma, print all of the numbers...
+with open("Python3\\Numbers.txt","r") as f:
+    data = f.read()
 
-
+    num = ""
+    for i in range(len(data)):
+        if(data[i]==","):
+            print(int(num)) # Typecasting string as integer.  
+            num = ""
+        else:
+            num += data[i]
 
 
