e81f6cbdee1aefb3f3d5e1e021f3199b045cee45
Rishikesh-001 <rishikeshraj4409@gmail.com>
2025-04-23 17:56:28 +0530

    Learned about calling constructors...

--- Oops Programming/Lecture_8.py
+++ Oops Programming/Lecture_8.py
@@ -1,6 +1,57 @@
-# I will start Lecture 8 today
+# <<<<<-----------------------------------------OOP's Programming------------------------------------------------->>>>>
 
-# WAP to count total number of elements in a list
+# Make a class of Students and store the name Rishikesh in it
+'''
+class Student:
+    name = "Rishikesh"
+    ERP = 606403
+    Section = "Section - P3"
 
-list = ["cow","goat","dog","cat"]
-print(len(list))
+s1 = Student()
+print(s1.name)
+print(s1.ERP)
+print(s1.Section)
+'''
+# Create a class and Object of a Car factory
+'''
+class Factory:
+    colour = "Blue"
+    Brand = "Mercedes"
+
+car1 = Factory()
+print(car1.Brand)
+print(car1.colour)
+'''
+
+# Create a class and a constructor for adding students
+
+class Student:
+    
+    def __init__(self,fullname):
+        self.name = fullname
+        print("adding new student in database...")
+
+s1 = Student("Rishikesh") #calling constructor
+print(s1.name)
+
+s1 = Student("Amritesh") #calling constructor
+print(s1.name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
