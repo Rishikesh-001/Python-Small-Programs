885e78f7d4e69a63fea73c3b967e4edc9cf57d51
Rishikesh-001 <rishikeshraj4409@gmail.com>
2025-04-25 02:04:49 +0530

    Leared more program concepts

--- Oops Programming/Lecture_8.py
+++ Oops Programming/Lecture_8.py
@@ -24,7 +24,7 @@ print(car1.colour)
 '''
 
 # Create a class and a constructor for adding students
-
+'''
 class Student:
     
     def __init__(self,fullname):
@@ -36,8 +36,143 @@ print(s1.name)
 
 s1 = Student("Amritesh") #calling constructor
 print(s1.name)
+'''
 
+# Constructor runs automatically upon calling 
+'''
+class Student:
+    def __init__(self):
+        print("Constructor running...")
 
+s1 = Student()
+'''
+
+# Create a class storing the name and mark of the student
+'''
+class Student:
+    def __init__(self,name,marks): # self is nothing but same as s1 and s2
+        self.name = name
+        self.marks = marks
+        print("Adding details to database...")
+
+s1 = Student("Rishikesh", 96)
+print(s1.name, s1.marks)
+
+s2 = Student("Amritesh", 98.9)
+print(s2.name, s2.marks)
+'''
+
+#<<<-----------------Types of Constructors----------------->>>
+'''
+# default constructors
+def __init__(self):
+    pass
+
+# parameterized constructors
+def __init__(self,name,marks): # self is nothing but same as s1 and s2
+        self.name = name
+        self.marks = marks
+        print("Adding details to database...")
+'''
+#<<<------------------------Attributes--------------------------->>>
+
+# Create a class storing the name and mark of the student
+'''
+class Student:
+    college_name = "Rungta College of Engineering and Technology"
+    def __init__(self,name,marks): # self is nothing but same as s1 and s2
+        self.name = name
+        self.marks = marks
+        print("Adding details to database...")
+
+s1 = Student("Rishikesh", 96)
+print(s1.name, s1.marks)
+print(s1.college_name)
+
+s2 = Student("Amritesh", 98.9)
+print(s2.name, s2.marks)
+print(Student.college_name)
+'''
+
+# Create a class storing the nme and marks of the students, set the default name to Anonymous
+'''
+class Student:
+    college_name = "RCET R1"
+    name = "anonymous" # class attribute
+    def __init__(self,name,marks): 
+        self.name = name # object attribute > class attribute
+        self.marks = marks
+        print("Adding details to database...")
+
+s1 = Student("Rishikesh", 96)
+print(s1.name, s1.marks)
+print(s1.college_name)
+
+s2 = Student("Amritesh", 98.9)
+print(s2.name, s2.marks)
+print(Student.college_name)
+'''
+
+# create a method to greet students and show their marks
+'''
+class Student:
+    college_name = "RCET R1"
+    name = "anonymous" 
+    def __init__(self,name,marks): 
+        self.name = name 
+        self.marks = marks
+
+    def welcome(self):
+        print("welcome student,", self.name)
+    
+    def get_marks(self):
+        return self.marks
+        
+
+s1 = Student("Rishikesh", 96)
+s1.welcome()
+print(s1.get_marks())
+'''
+
+# create student class that takes name and marks of 3 subjects as arguements in constructor. Then create a method to print the average
+'''
+class Student:
+    def __init__(self,name,marks):
+        self.name = name
+        self.marks = marks
+
+    def get_avg(self):
+        sum = 0
+        for val in self.marks:
+            sum += val
+        print("Hi,", self.name, "your average score is:", sum/3)
+
+s1 = Student("Rishikesh Raj", [99,90,92])
+s1.get_avg()
+
+s2 = Student("Amritesh Raj", [99.97,98])
+s2.get_avg()
+'''
+
+#<<<----------------------------Static Method------------------------------->>>
+
+# create a method to greet students with "hello"
+class Student:
+    college_name = "RCET R1"
+    @staticmethod # decorator
+    def hello():
+        print("Hello")
+
+    def __init__(self,name): 
+        self.name = name 
+
+    def welcome(self):
+        print("welcome student,", self.name)
+        
+
+s1 = Student("Rishikesh")
+s1.hello()
+s1.welcome()
 
 
 
