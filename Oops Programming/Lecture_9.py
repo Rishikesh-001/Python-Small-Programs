ccfd0723853ed382e1a98058e8a36a28c5373260
Rishikesh-001 <rishikeshraj4409@gmail.com>
2025-04-27 19:06:49 +0530

    Learned private attributes

--- Oops Programming/Lecture_9.py
+++ Oops Programming/Lecture_9.py
@@ -1,5 +1,5 @@
 # deleting an object 
-
+'''
 class Student:
     def __init__(self, name):
         self.name = name
@@ -8,3 +8,39 @@ s1 = Student("Rishikesh Raj")
 print(s1.name) # we will get our output
 del s1 # s1 or student 1 object is deleted 
 print(s1.name) # here we will get error
+'''
+# Private attributes
+'''
+class Account:
+    def __init__(self, acc_no, acc_pass):
+        self.acc_no = acc_no
+        self.__acc_pass = acc_pass # add 2 underscore in self.acc_pass to make it private
+
+acc1 = Account("12345","abcde")
+
+print(acc1.acc_no)
+print(acc1.__acc_pass) # it will give error
+'''
+# create a class which can access private attributes
+
+class Person:
+    __name = "anonymous"
+
+    def __hello(self):
+        print("hello person")
+    
+    def welcome(self):
+        self.__hello()
+
+p1 = Person()
+print(p1.welcome())
+
+
+
+
+
+
+
+
+
+
