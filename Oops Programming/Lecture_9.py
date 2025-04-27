72205fb03f7efa7baeb94823a858734d8dcf8b62
Rishikesh-001 <rishikeshraj4409@gmail.com>
2025-04-27 19:26:32 +0530

    Learned Inheritance of Class

--- Oops Programming/Lecture_9.py
+++ Oops Programming/Lecture_9.py
@@ -9,7 +9,10 @@ print(s1.name) # we will get our output
 del s1 # s1 or student 1 object is deleted 
 print(s1.name) # here we will get error
 '''
-# Private attributes
+
+# Private attributes: These are meant to be used only within the class and are not accessible from outside the class...
+
+# create a class with Private attribute
 '''
 class Account:
     def __init__(self, acc_no, acc_pass):
@@ -22,7 +25,7 @@ print(acc1.acc_no)
 print(acc1.__acc_pass) # it will give error
 '''
 # create a class which can access private attributes
-
+'''
 class Person:
     __name = "anonymous"
 
@@ -34,13 +37,31 @@ class Person:
 
 p1 = Person()
 print(p1.welcome())
+'''
 
+#<<<----------------------------------Inheritance------------------------------------>>>
 
+# Inheritace in classes: When one class (child/derived) derives the properties & methods of another class(parent/base)
 
+class Car:
+    color = "violet"
+    @staticmethod
+    def start():
+        print("Car started.")
 
+    @staticmethod
+    def stop():
+        print("Car stopped.")
 
+class ToyotaCar(Car):
+    def __init__(self, name):
+        self.name = name
 
+car1 = ToyotaCar("Fotuner")
+car2 = ToyotaCar("Camry")
 
-
-
+print(car1.name)
+print(car1.start())
+print(car1.stop())
+print(car1.color)
 
